b270fbd2f56924c506aecb3ca06e49fa70c81470
Denis Frolov <den-frolov38@yandex.ru>
2017-10-21 18:12:51 +0300

    full

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,30 @@ var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 var pngquant = require('imagemin-pngquant');
 var imageResize = require('gulp-image-resize');
 var cache = require('gulp-cache');
+var del          = require('del');
+
+// Images resize
+gulp.task('resize', function () {
+	gulp.src('src/**/*')
+	.pipe(imageResize({
+		width : 1920,
+		height : 1080,
+      //crop : true,
+      //upscale : false
+  }))
+	.pipe(gulp.dest('dist'));
+});
+
 
 // Images optimization and copy in /dist
 gulp.task('img', function() {
 	return gulp.src('src/**/*')
-	.pipe(imageResize({
-		width : 1920
-	})
-	.pipe(cache(imagemin([
+	.pipe(cache(imageResize({
+		width : 1920,
+      //crop : true,
+      //upscale : false
+  }),
+	imagemin([
 		imagemin.gifsicle({interlaced: true}),
 		imagemin.jpegtran({progressive: true}),
 		imageminJpegRecompress({
@@ -30,6 +46,9 @@ gulp.task('img', function() {
 });
 
 // Clearing the cache
-gulp.task('clear', function (done) {
+gulp.task('clean', function() {
+	return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+gulp.task('clear', ['clean'], function (done) {
 	return cache.clearAll(done);
 });
