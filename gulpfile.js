ee15936aa00eeb64541e8c41b72161401bb97a6a
Denis Frolov <den-frolov38@yandex.ru>
2017-11-07 21:32:21 +0300

    11.07.2017

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,37 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin'); 
+var gulp                   = require('gulp');
+var imagemin               = require('gulp-imagemin'); 
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-var pngquant = require('imagemin-pngquant');
-var imageResize = require('gulp-image-resize');
-var cache = require('gulp-cache');
-var del          = require('del');
-
-// Images resize
-gulp.task('resize', function () {
-	gulp.src('src/**/*')
-	.pipe(imageResize({
-		width : 1920,
-      //crop : true,
-      //upscale : false
-  }))
-	.pipe(gulp.dest('dist'));
-});
+var pngquant               = require('imagemin-pngquant');
+var imageResize            = require('gulp-image-resize');
+var cache                  = require('gulp-cache');
+var del                    = require('del');
 
 
 // Images optimization and copy in /dist
-gulp.task('img',['clear'], function() {
+gulp.task('img',['clean'], function() {
 	return gulp.src('src/**/*')
-	.pipe(cache(imageResize({
-		width : 1920,
-      //crop : true,
-      //upscale : false
-  }),
-	imagemin([
-		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
-		imageminJpegRecompress({
-			loops: 5,
-			min: 65,
-			max: 70,
-			quality:'medium'
-		}),
-		imagemin.svgo(),
-		imagemin.optipng({optimizationLevel: 3}),
-		pngquant({quality: '65-70', speed: 5})
-		],{
-			verbose: true
-		})))
+	.pipe(
+		imageResize({
+			width : 800,
+			height : 800,
+			//crop : true,
+      		//upscale : false
+      	}),
+		imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imageminJpegRecompress({
+				loops: 5,
+				min: 65,
+				max: 70,
+				quality:'medium'
+			}),
+			imagemin.svgo(),
+			imagemin.optipng({optimizationLevel: 3}),
+			pngquant({quality: '65-70', speed: 5})
+			],{
+				verbose: true
+			}))
 	.pipe(gulp.dest('dist'));
 });
 
@@ -48,6 +39,6 @@ gulp.task('img',['clear'], function() {
 gulp.task('clean', function() {
 	return del.sync('dist'); // Удаляем папку dist перед сборкой
 });
-gulp.task('clear', ['clean'], function (done) {
+gulp.task('clear', function (done) {
 	return cache.clearAll(done);
 });
