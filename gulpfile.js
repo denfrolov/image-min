088762f223cd323fe097b31958cfb6510da10cf9
Denis Frolov <mail@denfrolov.ru>
2018-06-23 02:57:54 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,48 @@
-var gulp                   = require('gulp');
-var imagemin               = require('gulp-imagemin'); 
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-var pngquant               = require('imagemin-pngquant');
-var imageResize            = require('gulp-image-resize');
-var del                    = require('del');
+var pngquant = require('imagemin-pngquant');
+var imageResize = require('gulp-image-resize');
+var del = require('del');
 
 // Images optimization and copy in /dist
-gulp.task('img',['clean'], function() {
-	return gulp.src('src/**/*')
-	//.pipe(imageResize({ width : 1920, height : 800, crop : false, upscale : false }))
-	.pipe(
-		imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.jpegtran({progressive: true}),
-			imageminJpegRecompress({
-				loops: 5,
-				min: 65,
-				max: 70,
-				quality:'medium'
-			}),
-			imagemin.svgo(),
-			imagemin.optipng({optimizationLevel: 3}),
-			pngquant({quality: '65-70', speed: 5})
-			],{
+gulp.task('img', ['clean'], function () {
+	return gulp.src(['src/**/*.jpg', 'src/**/*.png'])
+		.pipe(imageResize({
+			width: 1920,
+			//height: 800,
+			crop: false,
+			upscale: false
+		}))
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({
+					interlaced: true
+				}),
+				imagemin.jpegtran({
+					progressive: true
+				}),
+				imageminJpegRecompress({
+					loops: 5,
+					min: 75,
+					max: 92,
+					quality: 'medium'
+				}),
+				imagemin.svgo(),
+				imagemin.optipng({
+					optimizationLevel: 3
+				}),
+				pngquant({
+					quality: '75-92',
+					speed: 5
+				})
+			], {
 				verbose: true
 			}))
-	.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'));
 });
 
 // Clearing the cache and remove dist
-gulp.task('clean', function() {
+gulp.task('clean', function () {
 	return del.sync('dist'); // Удаляем папку dist перед сборкой
 });
